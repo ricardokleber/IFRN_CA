392b675d471167ca26aa641f9fe2db35b0affccc
Ricardo Kléber <ricardokleber@ricardokleber.com.br>
2024-03-19 09:23:39 -0300

    Delete PEOO_2024/Lista_01 directory

--- PEOO_2024/Lista_01/L02_004.py
+++
@@ -1,7 +0,0 @@
-lista = []
-arquivo = open('agenda.txt','r')
-for registro in arquivo.readlines():
-    lista.append(registro.replace('\n',''))
-arquivo.close()
-
-print(lista)
--- PEOO_2024/Lista_01/L02_005.py
+++
@@ -1,9 +0,0 @@
-dicionario = {}
-arquivo = open('agenda.txt','r')
-for registro in arquivo.readlines():
-    nome = registro.split(':')[0]
-    telefone = int((registro.split(':')[1]).replace('\n',''))
-    dicionario[telefone] = nome
-arquivo.close()
-
-print(dicionario)
